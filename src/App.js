a79d15c34adae3bf36379852d7f12a97a7133142
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-06 18:35:47 -0400

    style changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="card bg-dark p" style={{color:"#9e5a0c", padding:"5px 20px"}}>
+        {/* <img src="public/PorfolioPic.png" alt="Ocean Scene"></img> */}
+        <div className="card" style={{color:"#49859a", padding:"5px 20px"}}>
           <p>Marianna Bruns</p>
           <p>Full Stack Web Developer</p>
         </div>
