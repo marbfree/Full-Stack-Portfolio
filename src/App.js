0e827cd6e06d8977bccfb4329862cdc420832568
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-06 13:19:36 -0400

    playing around with styling and editing App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,18 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="card bg-dark p" style={{color:"#9e5a0c", padding:"5px 20px"}}>
+          <p>Marianna Bruns</p>
+          <p>Full Stack Web Developer</p>
+        </div>
       </header>
     </div>
   );
