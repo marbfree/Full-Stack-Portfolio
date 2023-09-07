da3401b02c7d8b4d78f2c6f810fb33cff59851c0
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-07 18:30:35 -0400

    changed navtab completely

--- src/components/NavBar/Navigation.js
+++ src/components/NavBar/Navigation.js
@@ -1,26 +1,50 @@
-import React, {Component} from "react";
-import { PortfolioContainer } from "../NavBar/Options";
-import '../NavBar/Navigation.css'
+import React from 'react';
 
-class Navbar extends Component {
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
 
-  render() {
-    return(
-      <nav className="NavbarOptions">
-        <ul style={{listStyleType: "none"}}>
-          {PortfolioContainer.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                {item.title}
-                </a>
-              </li>
-            )
-          })} 
-        </ul>
-      </nav>
-    )
-  }
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#bio"
+          onClick={() => handlePageChange('Bio')}
+
+          className={currentPage === 'Bio' ? 'nav-link active' : 'nav-link'}
+        >
+          Bio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#projects"
+          onClick={() => handlePageChange('Projects')}
+
+          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+        >
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
 }
 
-export default Navbar
+export default NavTabs;
