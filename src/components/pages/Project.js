78cf0b8982a40ca7efc7ef8901f023bc0dcc2780
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-08 13:21:41 -0400

    troubleshooting css

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -3,20 +3,22 @@ import '../pages/Project.css'
 
 export default function Projects() {
   return (
+    <body className="background-project">
     <div>
       <h1>Projects</h1>
       <section>
-        {/* <a href='#'>
+        <a href='#'>
             <img src='' alt='Peaceful Mountains'></img>
             <p>Project Description</p>
-        </a> */} 
         Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
         Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
         mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
         lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
         imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
         in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        </a>
       </section>
     </div>
+    </body>
   );
 }
