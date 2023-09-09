930352fb31a5663a275c4276a0543750475d3a5a
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-09 14:18:42 -0400

    added styling to contact page:

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,25 +4,27 @@ import styles from '../../components/pages/styles.module.css'
 export default function Contact() {
   return (
     <section className={styles.background}>
+    <div className={styles.text}>Marianna Bruns</div>
+    <div className={styles.text}>Email: mariannabfree@gmail.com</div>
+    <div className={styles.text}>Phone: (404) 769-8522</div>
     <div className="container">
     <form action="action_page.php">
+    
+    <h2 className={styles.title}>Contact</h2>
+      <label for="fname"></label>
+      <input style={{marginBottom:"10px", marginRight:"10px", marginTop:"0px"}} type="text" id="fname" name="firstname" placeholder="First Name"></input>
   
-      <label for="fname">First Name</label>
-      <input type="text" id="fname" name="firstname" placeholder="Your name.."></input>
+      <label for="lname"></label>
+      <input type="text" id="lname" name="lastname" placeholder="Last Name"></input>
   
-      <label for="lname">Last Name</label>
-      <input type="text" id="lname" name="lastname" placeholder="Your last name.."></input>
+      <label for="subject"></label>
+      <textarea id="subject" name="subject" placeholder="Write Message" style={{height:'200px', width: '500px'}}></textarea>
   
-      
-  
-      <label for="subject">Message</label>
-      <textarea id="subject" name="subject" placeholder="Write Message
-      " style={{height:'200px'}}></textarea>
-  
-      <input type="submit" value="Submit"></input>
-  
+      <input className={styles.submit} type="submit" value="Submit"></input>
+     
     </form>
   </div>
   </section>
+  
   );
 };
