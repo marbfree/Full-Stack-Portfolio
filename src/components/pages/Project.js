329356870e258d2dee9b54abe0444bb2b6b6dd06
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-09 13:52:23 -0400

    working on project cards

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -4,20 +4,17 @@ import project from '../pages/project.module.css'
 
 export default function Projects() {
     return (
-        <body className={project.background}>
-          <div>
+        <body className={project.body}>
             <h1 className={styles.heading}>Projects</h1>
-            <section className="card">
-                <div className="card" style={{width: "18rem"}}>
-                    <a href="#">
+            <a href="">
+                <div className={project.card} style={{width: "18rem"}}>
                     <img className="card-img-top" src="" alt="Card image cap"></img>
-                    <div className="card-body">
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                    </a>
+                        <div className="card-body">
+                            <h5 className={project.cardTitle}>Card Title</h5>
+                            <div className={project.cardText}>Some quick example text to build on the card title and make up the bulk of the card's content.</div>
+                        </div>
                 </div>
-            </section>
-          </div>
+            </a>
         </body>
     );
 }
