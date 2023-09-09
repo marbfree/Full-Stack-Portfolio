4a0feb227929075d5d0ef352a0e90b57de81ac24
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-09 16:03:12 -0400

    created footer links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PortfolioContainer from './components/layout/PortfolioContainer';
-import Footer from "./components/layout/Footer"
+import Footer from "./components/layout/Footer";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(fas, faFontAwesome)
 
 function App() {
   return (
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,14 +1,25 @@
 import React from "react";
 import '../layout/Footer.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 
 const Footer = () => {
     return(
         <>
 
         <footer className="footer">
-            <div style={{color: '#084961'}}>Marianna Bruns</div>
+            {/* <div style={{color: '#084961'}}>Marianna Bruns</div> */}
+            <a href="https://github.com/marbfree">GitHub&#124;
             <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+            <a href="https://www.linkedin.com/in/mariannabruns/">LinkedIn&#124;
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+            <a href="https://stackoverflow.com/users/22529498/marianna-b">StackOverflow
+            <FontAwesomeIcon icon="fa-brands fa-github" />
+            </a>
+            
         </footer>
         </>
     )
