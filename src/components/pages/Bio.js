9b2435a222264c8e13a61203db2fdcca855f074e
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-12 07:31:54 -0400

    styling projects page

--- src/components/pages/Bio.js
+++ src/components/pages/Bio.js
@@ -13,14 +13,14 @@ export default function Bio() {
     <div className="col-md-8">
       <img src={logo} className="img-fluid rounded-circle" alt="Headshot of Marianna Bruns"></img>
     </div>
-    <div className="col-md-8">
-      <div className="body" style={{color: "#084961"}}>
+    {/* <div className="col-md-8"> */}
+      <div style={{color: "#084961"}}>
         <h5 style={{fontSize: '2em'}}>Marianna Bruns</h5>
         <p>Versatile Full Stack Web Developer merging skills in web development and psychology. Adept at managing the entire development lifecycle while leveraging analytics from psychology to enhance user experiences. Committed to innovation, collaboration, and mastering emerging technologies.   Encouraging creative solutions to challenges utilizing research and development and over 10 years of management experience.</p>
       </div>
     </div>
   </div>
-</div>
+{/* </div> */}
     </div>
     </body>
   );
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -58,11 +58,12 @@ export default function Projects() {
     const listItems = projects.map(projects => (
         
           <li key={projects.id}>
-                <div className={project.card} style={{ width: "18rem" }}>
-                <div><a href={projects.url}><img src={projects.image} alt={projects.title}></img></a></div>
+       
+                <div className={project.card} style={{ width: "18rem", height: "15rem" }}>
+                <div class="container"><a href={projects.url}><img src={projects.image} alt={projects.title}></img></a></div>
                     <div className={project.cardText}>{projects.title}</div>
                 <a href={projects.repository}><button>Github</button></a>
-                </div>
+                </div> 
           </li>
         
     ))
