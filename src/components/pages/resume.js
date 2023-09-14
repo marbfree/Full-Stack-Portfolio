572ed22d9a60608a2fe7b6d56065538d47b70973
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-14 10:14:57 -0400

    updatd resume link

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -9,7 +9,7 @@ const Resume = () => {
       <div>
       <div className={styles.fadeInHello}><h2 className={styles.heading} style={{ color: "#eea6be" }}>Resume</h2></div>
         <button className={resume.button}>
-        <a style={{color: "#eea6be", padding: "20px"}} href="https://drive.google.com/file/d/1kyOi_dkdDrqdE_uGKaNuUpF9kVSt-0Gn/view?usp=sharing" download="Marianna Bruns Resume">Download Resume</a>
+        <a style={{color: "#eea6be", padding: "20px"}} href="https://drive.google.com/file/d/1kyOi_dkdDrqdE_uGKaNuUpF9kVSt-0Gn/view?usp=drive_link" download="Marianna Bruns Resume">Download Resume</a>
         </button>
         <div className="container">
           <p className={resume.text}>HTML | CSS | JavaScript | jQuery | node.js | express.js | mySQL | mongoDB | React | Debugging | Microsoft Word | Excel | Powerpoint | Zoom | Slack
