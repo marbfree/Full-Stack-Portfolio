503ecd1244ede920d3bf24e62f6564a2a043261b
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-09-17 08:07:55 -0400

    messing around with a modal on resume page

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,6 +1,7 @@
 import React from "react";
-import styles from '../pages/styles.module.css'
-import resume from './resume.module.css'
+import styles from '../pages/styles.module.css';
+import resume from './resume.module.css';
+import Modal from './Modal';
 
 
 const Resume = () => {
@@ -8,9 +9,10 @@ const Resume = () => {
     <body className={resume.background}>
       <div>
       <div className={styles.fadeInHello}><h2 className={styles.heading} style={{ color: "#eea6be" }}>Resume</h2></div>
-        <button className={resume.button}>
+       <Modal></Modal>
+        {/* <button className={resume.button}>
         <a style={{color: "#eea6be", padding: "20px"}} href="https://drive.google.com/file/d/1kyOi_dkdDrqdE_uGKaNuUpF9kVSt-0Gn/view?usp=drive_link" download="Marianna Bruns Resume">Download Resume</a>
-        </button>
+        </button> */}
         <div className="container">
           <p className={resume.text}>HTML | CSS | JavaScript | jQuery | node.js | express.js | mySQL | mongoDB | React | Debugging | Microsoft Word | Excel | Powerpoint | Zoom | Slack
           </p>
